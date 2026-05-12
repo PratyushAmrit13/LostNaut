8d7c979741b19cf06c6a1a84ccf06d879c78f49f
PratyushAmrit13 <pratyushamrit13@gmail.com>
2026-05-12 11:18:09 +0530

    UI works

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private void Update()
+    {
+        if(Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = isPaused ? 0f : 1f;
+
+        Cursor.visible = true;
+        Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+
+    public void OnClickResume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+}
