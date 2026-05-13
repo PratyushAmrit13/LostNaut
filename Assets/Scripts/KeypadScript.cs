95dbfb7834ad2af1bdc59e7607a08cce0ccaf677
PratyushAmrit13 <pratyushamrit13@gmail.com>
2026-05-13 11:43:54 +0530

    Keypad Update

--- Assets/Scripts/KeypadScript.cs
+++ Assets/Scripts/KeypadScript.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class KeypadScript : MonoBehaviour
 {
 
     [SerializeField] private TextMeshProUGUI ans;
+    [SerializeField] private int cans = 123;
     
     public void Number (int number)
     {
         ans.text += number.ToString();
+    }
+
+    public void Excecute()
+    {
+        if(ans.text == cans.ToString())
+        {
+            ans.text = "CORRECT";
+        }
+        else
+        {
+            ans.text = "INVALID";
+        }
+
+        StartCoroutine(ClearText());
+    }
+
+    IEnumerator ClearText()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ans.text = "";
     }
 }
